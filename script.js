f0b6ceb36f917a425557053526ddb5734a09d038
Lavenzo Kevin <livboy@yahoo.com>
2026-09-12 19:33:40 +0800

    Integrate Stage 4 background artwork

--- script.js
+++ script.js
@@ -27,6 +27,8 @@ const stage2Background = new Image();
 stage2Background.src = "image/Stage2Background.png";
 const stage3Background = new Image();
 stage3Background.src = "image/Stage3Background.png";
+const stage4Background = new Image();
+stage4Background.src = "image/Stage4Background.png";
 
 let language = "en";
 try {
@@ -5320,6 +5322,17 @@ class Scenery {
 
       c.drawImage(stage3Background, offset, 0, scaledW, H);
       c.drawImage(stage3Background, offset + scaledW, 0, scaledW, H);
+    } else if (g.stage.index === 3 && stage4Background.naturalWidth > 0) {
+      const scale = H / 724;
+      const scaledW = 2172 * scale;
+
+      const parallaxFactor = 0.6;
+      let x = -camera * parallaxFactor;
+      let offset = x % scaledW;
+      if (offset > 0) offset -= scaledW;
+
+      c.drawImage(stage4Background, offset, 0, scaledW, H);
+      c.drawImage(stage4Background, offset + scaledW, 0, scaledW, H);
     } else {
       const sky = c.createLinearGradient(0, 0, 0, TOP);
       sky.addColorStop(0, "#090f23");
